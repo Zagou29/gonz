a4dff3ad14dc460fe9471ba77b36e0354c06d5c6
Gonzague de Poulpiquet <gdepoulpiquet@me.com>
2025-01-16 06:50:11 +0100

    maj options photos

--- photos.js
+++ photos.js
@@ -421,7 +421,7 @@ aff_an.textContent = list_img[0].dataset.an;
 /* un observer pour afficher les dates dans la timeline verticale */
 let options = {
   root: null,
-  rootMargin: "0% -10% -100% -98%",
+  rootMargin: "0% -2% -100% -98%",
   threshold: 0,
 };
 const guette = new IntersectionObserver(affiche_date, options);
