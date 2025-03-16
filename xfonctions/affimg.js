28494e1b2d928ddc817c53938a3ad36f70188c58
Gonzague de Poulpiquet <gdepoulpiquet@me.com>
2025-03-16 10:34:34 +0100

    ameliorations de affimg et affvid par Copilot

--- xfonctions/affimg.js
+++ xfonctions/affimg.js
@@ -1,121 +1,218 @@
 import { cloneTemplate } from "./dom.js";
 
+/**
+ * Classe de gestion d'affichage des images et dates
+ * @class Affimg
+ */
 export class Affimg {
-  #listimg; //liste des objets img venat de JSON
-  #opt; // option 'photo' ou non
-  #asp; //show ou show_mod
-  #elt_images; //fragment où charger les img
-  #elt_dates; //element ou charger les liensdates
-  #ancre_imgs; //Boite où charger les images
-  #ancres_dates; //Boite où charger les Li dates
-  #image; //images à afficher
-  #lien_date; //liens dates à afficher
+  #listimg; // Liste des objets img venant de JSON
+  #opt; // Option 'photo' ou non
+  #asp; // show ou show_mod (aspect)
+  #elt_images; // Fragment où charger les img
+  #elt_dates; // Element où charger les liensdates
+  #ancre_imgs; // Boite où charger les images
+  #ancres_dates; // Boite où charger les Li dates
 
+  /**
+   * Crée une nouvelle instance d'Affimg
+   * @param {Array} listimg - Liste des objets images du JSON
+   * @param {string} opt - Option d'affichage ('photo' ou autre)
+   * @param {string} asp - Aspect des images ('show' ou 'show_mod')
+   */
   constructor(listimg, opt, asp) {
     this.#listimg = listimg;
     this.#opt = opt;
     this.#asp = asp;
-    /* construire this.#elt_images */
+
+    this.#preparerImages();
+    this.#preparerDates();
+  }
+
+  /**
+   * Prépare les éléments d'images
+   * @private
+   */
+  #preparerImages() {
     this.#elt_images = new DocumentFragment();
     let n = 0;
     let vseuil = "";
-    this.#listimg[0].seuil = vseuil;
-    if (this.#opt === "photo") {
-      this.#listimg.forEach((obj, index) => {
-        if (obj.an !== vseuil) {
-          obj.seuil = obj.an;
-          obj.num = index;
-          n = index;
-        } else {
-          obj.num = n;
-          obj.seuil = "";
-        }
-        this.#image = new AffItem(obj, this.#asp);
-        this.#elt_images.append(this.#image.retourImage);
-        vseuil = obj.an;
-      });
-    } else {
-      let ind = 0;
-      this.#listimg.forEach((obj, index) => {
-        if (obj.an !== vseuil) {
-          obj.seuil = obj.an;
-          //s'il y a moins de 2 seuil "" apres le précédent non vide , forcer seuil à "", sinon recaler ind sur l'index de cette ligne
-          index - ind < 4 && index - ind > 0 ? (obj.seuil = "") : (ind = index);
-        } else {
-          obj.seuil = "";
-        }
-        obj.num = index;
-        this.#image = new AffItem(obj, this.#asp);
-        this.#elt_images.append(this.#image.retourImage);
-        vseuil = obj.an;
-      });
+
+    // S'assurer que le premier élément a une valeur seuil initiale
+    if (this.#listimg.length > 0) {
+      this.#listimg[0].seuil = vseuil;
     }
-    /* construire this.#elt_dates */
-    this.#elt_dates = new DocumentFragment();
+
     if (this.#opt === "photo") {
-      this.#listimg.forEach((obj) => {
-        this.#lien_date = new DateItem(obj);
-        obj.seuil !== ""
-          ? this.#elt_dates.append(this.#lien_date.retourDateItem)
-          : "";
-        });
-      } else {
-        this.#listimg.forEach((obj) => {
-          this.#lien_date = new DateItem(obj);
-          this.#elt_dates.append(this.#lien_date.retourDateItem);
-        });
-      }
+      this.#preparerImagesPhoto(vseuil);
+    } else {
+      this.#preparerImagesAutres(vseuil);
+    }
   }
 
-  /* inclure dans les ancres adequates */
+  /**
+   * Prépare les images pour l'option 'photo'
+   * @param {string} vseuil - Valeur seuil initiale
+   * @private
+   */
+  #preparerImagesPhoto(vseuil) {
+    let n = 0;
+    this.#listimg.forEach((obj, index) => {
+      if (obj.an !== vseuil) {
+        obj.seuil = obj.an;
+        obj.num = index;
+        n = index;
+      } else {
+        obj.num = n;
+        obj.seuil = "";
+      }
+
+      const image = new AffItem(obj, this.#asp);
+      this.#elt_images.append(image.retourImage);
+      vseuil = obj.an;
+    });
+  }
+
+  /**
+   * Prépare les images pour les options autres que 'photo'
+   * @param {string} vseuil - Valeur seuil initiale
+   * @private
+   */
+  #preparerImagesAutres(vseuil) {
+    let ind = 0;
+    this.#listimg.forEach((obj, index) => {
+      if (obj.an !== vseuil) {
+        obj.seuil = obj.an;
+        // S'il y a moins de 4 éléments après le précédent non vide, forcer seuil à "",
+        // sinon recaler ind sur l'index de cette ligne
+        if (index - ind < 4 && index - ind > 0) {
+          obj.seuil = "";
+        } else {
+          ind = index;
+        }
+      } else {
+        obj.seuil = "";
+      }
+
+      obj.num = index;
+      const image = new AffItem(obj, this.#asp);
+      this.#elt_images.append(image.retourImage);
+      vseuil = obj.an;
+    });
+  }
+
+  /**
+   * Prépare les éléments de dates
+   * @private
+   */
+  #preparerDates() {
+    this.#elt_dates = new DocumentFragment();
+
+    if (this.#opt === "photo") {
+      // Pour l'option photo, n'ajouter que les éléments avec un seuil non vide
+      this.#listimg.forEach((obj) => {
+        if (obj.seuil !== "") {
+          const lien_date = new DateItem(obj);
+          this.#elt_dates.append(lien_date.retourDateItem);
+        }
+      });
+    } else {
+      // Pour les autres options, ajouter tous les éléments
+      this.#listimg.forEach((obj) => {
+        const lien_date = new DateItem(obj);
+        this.#elt_dates.append(lien_date.retourDateItem);
+      });
+    }
+  }
+
+  /**
+   * Injecte les images dans l'élément d'ancrage
+   * @param {HTMLElement} ancre_imgs - L'élément DOM où injecter les images
+   */
   creeimages(ancre_imgs) {
     this.#ancre_imgs = ancre_imgs;
     this.#ancre_imgs.append(this.#elt_images);
   }
 
+  /**
+   * Injecte les dates dans l'élément d'ancrage
+   * @param {HTMLElement} ancres_dates - L'élément DOM où injecter les dates
+   */
   creedates(ancres_dates) {
     this.#ancres_dates = ancres_dates;
     this.#ancres_dates.append(this.#elt_dates);
   }
 }
 
+/**
+ * Classe pour créer un élément image
+ * @class AffItem
+ */
 class AffItem {
   #imgobj;
   #el_image;
   #asp;
+
+  /**
+   * @param {Object} imgobj - Objet image à afficher
+   * @param {string} asp - Classe CSS à appliquer
+   */
   constructor(imgobj, asp) {
     this.#imgobj = imgobj;
     this.#asp = asp;
     this.#el_image = cloneTemplate("photos").firstElementChild;
+
+    // Configuration de l'élément image
     this.#el_image.setAttribute("src", this.#imgobj.src);
     this.#el_image.setAttribute("alt", this.#imgobj.an);
     this.#el_image.setAttribute("class", this.#asp);
     this.#el_image.dataset.an = this.#imgobj.an;
     this.#el_image.dataset.num = this.#imgobj.num;
-    this.#imgobj.seuil !== ""
-      ? (this.#el_image.dataset.seuil = this.#imgobj.seuil)
-      : "";
+
+    // Ajouter l'attribut seuil si nécessaire
+    if (this.#imgobj.seuil !== "") {
+      this.#el_image.dataset.seuil = this.#imgobj.seuil;
+    }
   }
 
+  /**
+   * Retourne l'élément image créé
+   * @return {HTMLElement}
+   */
   get retourImage() {
     return this.#el_image;
   }
 }
 
+/**
+ * Classe pour créer un élément de date
+ * @class DateItem
+ */
 class DateItem {
   #dateObj;
   #dateElt;
+
+  /**
+   * @param {Object} dateObj - Objet contenant les informations de date
+   */
   constructor(dateObj) {
     this.#dateObj = dateObj;
     this.#dateElt = cloneTemplate("liendate").firstElementChild;
+
+    // Configuration de l'élément date
     this.#dateElt.dataset.an = this.#dateObj.an;
     this.#dateElt.dataset.num = this.#dateObj.num;
     this.#dateElt.textContent = this.#dateObj.an;
-    this.#dateObj.seuil !== ""
-      ? (this.#dateElt.dataset.seuil = this.#dateObj.an)
-      : "";
+
+    // Ajouter l'attribut seuil si nécessaire
+    if (this.#dateObj.seuil !== "") {
+      this.#dateElt.dataset.seuil = this.#dateObj.an;
+    }
   }
 
+  /**
+   * Retourne l'élément date créé
+   * @return {HTMLElement}
+   */
   get retourDateItem() {
     return this.#dateElt;
   }
--- xfonctions/affvid.js
+++ xfonctions/affvid.js
@@ -1,69 +1,118 @@
 import { cloneTemplate } from "./dom.js";
+
 /**
- * @typedef {object} boxes
- * @property {string} ec
- * @property {string} id
- * @property {string} class
- * @property {string} text
+ * Définition d'un objet vidéo
+ * @typedef {object} VideoItem
+ * @property {string} ec - Format de l'écran (43 ou 169)
+ * @property {string} id - Identifiant YouTube
+ * @property {string} clas - Classes CSS associées
+ * @property {string} text - Texte descriptif
+ * @property {string} annee - Année de la vidéo
+ */
+
+/**
+ * Gère l'affichage des vidéos et des miniatures YouTube
+ * @class Affvid
  */
 export class Affvid {
-  /**
-   * @type {array}vidlist avec ec,id,class,text
-   *  */
+  /** @type {VideoItem[]} - Liste complète des vidéos */
   #vidlist = [];
+  /** @type {VideoItem[]} - Vidéos filtrées selon les critères actuels */
   #vidSelect = [];
+  /** @type {DocumentFragment} - Fragment pour les vidéos */
   #listElement;
+  /** @type {DocumentFragment} - Fragment pour les barres */
   #listElem;
+  /** @type {HTMLElement} - Conteneur pour les vidéos */
   #container;
+  /** @type {string} - Classe CSS pour le filtrage */
   #classe = "";
+  /** @type {HTMLElement} - Menu pour les barres */
   #menu;
+  /** @type {VideoItem[]} - Liste triée des vidéos et diaporamas */
   #liste = [];
+  /** @type {HTMLElement} - Liste des années */
   #ul_Years;
+  /** @type {Set<string>} - Années disponibles */
   #an_Select;
+  /** @type {DocumentFragment} - Fragment pour les éléments d'année */
   #li_Annee;
+  /** @type {string} - Année sélectionnée */
   #an;
+  /** @type {string} - ID de la vidéo sélectionnée */
   #vidId;
+  /** @type {string} - ID du template à utiliser */
   #tempId;
+  /** @type {HTMLElement} - Élément image de la vidéo */
   #vidImg;
+  /** @type {VidItem} - Instance de VidItem */
   #video;
+  /** @type {HTMLElement} - Élément iFrame de la vidéo */
   #imgVid;
+  /** @type {BarItem} - Instance de BarItem */
   #barItem;
+
+  // Dimensions et ratios
+  /** @type {number} - Largeur du conteneur */
   #wl;
+  /** @type {number} - Hauteur du conteneur */
   #wh;
+  /** @type {number} - Ratio de l'image */
   #ratioI;
+  /** @type {number} - Ratio du conteneur */
   #ratioW;
+  /** @type {AnnItem} - Instance de AnnItem */
   #an_Item;
 
   /**
-   * tableau des videos objets
-   * @param {vidlist[]} vidlist
+   * Initialise le gestionnaire de vidéos
+   * @param {VideoItem[]} vidlist - Liste des objets vidéo
    */
   constructor(vidlist) {
     this.#vidlist = vidlist;
   }
+
   /**
-   * @param {HTMLElement} element (ecVideos)
-   * @param {string} clas (classes selectionné via le lien menu Li)
+   * Affiche les miniatures des vidéos dans le conteneur
+   * @param {HTMLElement} container - Conteneur pour les vidéos
+   * @param {string} classe - Classe CSS pour filtrer
+   * @param {string} [an] - Année pour filtrer
+   * @param {string} tempId - ID du template à utiliser
    */
-  // charger les Thumbs dans ecVideos
   affVideos(container, classe, an, tempId) {
-    this.#tempId = tempId;
+    if (!container || !(container instanceof HTMLElement)) {
+      console.error("Le conteneur doit être un élément HTML valide");
+      return;
+    }
+
+    this.#tempId = tempId || "ytThumb";
     this.#container = container;
-    this.#classe = classe;
+    this.#classe = classe || "";
     this.#an = an;
     this.#listElement = new DocumentFragment();
-    /* préparer vidSelect selon la classe ou l'année */
+
+    this.#filtrerVideos();
+    this.#creerThumbnails();
+
+    this.#container.append(this.#listElement);
+  }
+
+  /**
+   * Filtre les vidéos selon la classe et/ou l'année
+   * @private
+   */
+  #filtrerVideos() {
     if (this.#an) {
-      /* si on déselectionne video ou diapo dans onglet annee*/
+      /* Si on déselectionne video ou diapo dans onglet année */
       if (this.#classe.length > 4) {
         this.#classe = this.#classe.slice(0, 4);
       } else {
         this.#classe = "";
       }
-      /* si this.#clas="", le filtre prend tout */
-      /* ne prend pas les playlist */
+
+      /* Filtre par année, classe et longueur d'ID (exclut les playlists) */
       this.#vidSelect = this.#vidlist
-        .filter((obj) => obj.annee === an)
+        .filter((obj) => obj.annee === this.#an)
         .filter((obj) => obj.clas.includes(this.#classe))
         .filter((obj) => obj.id.length < 12);
     } else {
@@ -71,169 +120,298 @@ export class Affvid {
         obj.clas.includes(this.#classe)
       );
     }
+
+    /* Trier par type: vidéos puis diaporamas */
     this.#liste = [
       ...this.#vidSelect.filter((item) => item.clas.includes(".vid")),
       ...this.#vidSelect.filter((item) => item.clas.includes(".dia")),
     ];
+  }
+
+  /**
+   * Crée les miniatures pour chaque vidéo
+   * @private
+   */
+  #creerThumbnails() {
     this.#liste.forEach((obj, index) => {
-      // toujours thumnail pour l'affichage de depart des videos
       this.#video = new VidItem(obj, this.#tempId);
       this.#vidImg = this.#video.retourItem.querySelector(".vidImg");
-      this.#vidImg.setAttribute("width", this.#setDim(this.#container, obj)[0]);
-      this.#vidImg.setAttribute(
-        "height",
-        this.#setDim(this.#container, obj)[1]
-      );
+
+      // Configurer les dimensions
+      const [largeur, hauteur] = this.#setDim(this.#container, obj);
+      this.#vidImg.setAttribute("width", largeur);
+      this.#vidImg.setAttribute("height", hauteur);
+
+      // Ajouter l'index pour la navigation
       this.#video.retourItem.querySelector(".lect").dataset.num = index;
       this.#listElement.append(this.#video.retourItem);
     });
-
-    this.#container.append(this.#listElement);
   }
-  // charger une ytFrameR après un titre VidTitre
+
+  /**
+   * Affiche une vidéo en mode lecture
+   * @param {HTMLElement} container - Conteneur pour la vidéo
+   * @param {string} vidId - ID de la vidéo YouTube
+   */
   aff_ytFrameR(container, vidId) {
+    if (!container || !vidId) return;
+
     this.#container = container;
     this.#vidId = vidId;
     this.#vidSelect = this.#vidlist.find((item) => item.id === this.#vidId);
+
     if (this.#vidSelect) {
       this.#video = new VidItem(this.#vidSelect, "ytFrameR");
       this.#imgVid = this.#video.retourItem.querySelector(".vidImg");
-      this.#imgVid.setAttribute(
-        "width",
-        this.#setDim(this.#container.parentElement, this.#vidSelect)[0]
+
+      // Configurer les dimensions
+      const [largeur, hauteur] = this.#setDim(
+        this.#container.parentElement,
+        this.#vidSelect
       );
-      this.#imgVid.setAttribute(
-        "height",
-        this.#setDim(this.#container.parentElement, this.#vidSelect)[1]
-      );
+      this.#imgVid.setAttribute("width", largeur);
+      this.#imgVid.setAttribute("height", hauteur);
+
+      // Activer la lecture automatique
       this.#imgVid.src = this.#imgVid.src.replace("autoplay=0", "autoplay=1");
 
       this.#container.append(this.#video.retourItem);
     }
   }
 
-  // charger les barres des videos/Thumb sous le menu
+  /**
+   * Affiche la barre des vidéos sous le menu
+   * @param {HTMLElement} menu - Élément menu pour les barres
+   */
   affBar(menu) {
-    if (this.#liste.length > 1) {
-      this.#menu = menu;
-      this.#listElem = new DocumentFragment();
-      this.#liste.forEach((obj, index) => {
-        this.#barItem = new BarItem(obj);
-        this.#barItem.retourBarItem.dataset.num = index;
-        this.#listElem.append(this.#barItem.retourBarItem);
-      });
-      this.#menu.append(this.#listElem);
-    }
+    if (!this.#liste.length || this.#liste.length <= 1) return;
+
+    this.#menu = menu;
+    this.#listElem = new DocumentFragment();
+
+    this.#liste.forEach((obj, index) => {
+      this.#barItem = new BarItem(obj);
+      this.#barItem.retourBarItem.dataset.num = index;
+      this.#listElem.append(this.#barItem.retourBarItem);
+    });
+
+    this.#menu.append(this.#listElem);
   }
-  // pour compter le nbre de videos dans vidscript
+
+  /**
+   * Récupère la liste des vidéos sélectionnées
+   * @return {VideoItem[]} Liste des vidéos sélectionnées
+   */
   get retourVideo() {
     return this.#vidSelect;
   }
+
+  /**
+   * Affiche les années disponibles
+   * @param {HTMLElement} ul_Years - Conteneur pour la liste des années
+   */
   aff_ans(ul_Years) {
+    if (!ul_Years) return;
+
     this.#ul_Years = ul_Years;
     this.#li_Annee = new DocumentFragment();
+
+    // Ne conserver que les vidéos (exclure les playlists) et extraire les années uniques
     this.#an_Select = new Set(
       this.#vidlist.filter((obj) => obj.id.length < 12).map((it) => it.annee)
     );
-    this.#an_Select.forEach((obj) => {
-      this.#an_Item = new AnnItem(obj);
+
+    // Créer les éléments d'année
+    this.#an_Select.forEach((annee) => {
+      this.#an_Item = new AnnItem(annee);
       this.#li_Annee.append(this.#an_Item.retourAnnItem);
     });
+
     this.#ul_Years.append(this.#li_Annee);
   }
-  // calcul des dimensions des videos/thumbs
-  #setDim(ecrans, item) {
-    this.#wl = ecrans.clientWidth - 5;
-    this.#wh = ecrans.clientHeight - 27;
-    this.#ratioI = item.ec === "43" ? 4 / 3 : 16 / 9;
+
+  /**
+   * Calcule les dimensions optimales pour une vidéo
+   * @param {HTMLElement} conteneur - Conteneur parent
+   * @param {VideoItem} video - Objet vidéo
+   * @return {[number, number]} Tableau [largeur, hauteur]
+   * @private
+   */
+  #setDim(conteneur, video) {
+    // Ajuster les marges
+    const MARGE_LARGEUR = 5;
+    const MARGE_HAUTEUR = 27;
+
+    this.#wl = conteneur.clientWidth - MARGE_LARGEUR;
+    this.#wh = conteneur.clientHeight - MARGE_HAUTEUR;
+
+    // Définir le ratio de l'image selon le format (4:3 ou 16:9)
+    this.#ratioI = video.ec === "43" ? 4 / 3 : 16 / 9;
+    // Ratio du conteneur
     this.#ratioW = this.#wl / this.#wh;
-    /* si on compare les ratios,il faut inverser et definir d'abord la hauteur */
-    /* return [ width(wl), height(wh)]*/
-    return [
-      this.#ratioW > this.#ratioI
-        ? Math.floor(this.#wh * this.#ratioI)
-        : this.#wl,
-      this.#ratioW > this.#ratioI
-        ? this.#wh
-        : Math.floor(this.#wl / this.#ratioI),
-    ];
+
+    // Si le ratio du conteneur est plus grand que celui de l'image,
+    // on ajuste la largeur en fonction de la hauteur, sinon l'inverse
+    if (this.#ratioW > this.#ratioI) {
+      return [Math.floor(this.#wh * this.#ratioI), this.#wh];
+    } else {
+      return [this.#wl, Math.floor(this.#wl / this.#ratioI)];
+    }
   }
 }
-// création d'un Thumb ou d'un Iframe video
+
+/**
+ * Crée un élément miniature ou iframe de vidéo
+ * @class VidItem
+ */
 class VidItem {
+  /** @type {string} - ID du template à utiliser */
   #tempId;
+  /** @type {VideoItem} - Objet vidéo */
   #vidItem;
+  /** @type {HTMLElement} - Élément DOM créé */
   #vidElement;
+  /** @type {string} - Classe de la vidéo (video/diapo) */
   #vidClass;
+  /** @type {HTMLElement} - Élément titre */
   #vidTitre;
+  /** @type {HTMLElement} - Élément vidéo/image */
   #video;
-  // tempId est le template a utiliser ytThumb ou ytFrame
-  constructor(vid, tempId) {
-    this.#tempId = tempId;
-    this.#vidItem = vid;
+
+  /**
+   * Crée un élément vidéo
+   * @param {VideoItem} video - Objet vidéo
+   * @param {string} tempId - ID du template à utiliser (ytThumb/ytFrame/ytFrameR)
+   */
+  constructor(video, tempId) {
+    this.#vidItem = video;
+    this.#tempId = tempId || "ytThumb";
     this.#vidElement = cloneTemplate(this.#tempId);
-    // preparer le span titre seulement pour AffVideos
-    if (this.#tempId === "ytThumb" || this.#tempId === "ytFrame") {
-      this.#vidTitre = this.#vidElement.querySelector(".vidTitre");
-      this.#vidClass = `${
-        this.#vidItem.clas.slice(0, 4) === ".vid" ? "video" : "diapo"
-      }`;
-      this.#vidTitre.textContent = `${
-        this.#vidClass.charAt(0).toUpperCase() + this.#vidClass.slice(1)
-      } ${this.#vidItem.text}`;
-      this.#vidTitre.classList.add(this.#vidClass);
-    }
+
+    this.#configurerTitre();
+    this.#configurerVideo();
+  }
+
+  /**
+   * Configure le titre de la vidéo si nécessaire
+   * @private
+   */
+  #configurerTitre() {
+    // Le titre n'est requis que pour les templates ytThumb et ytFrame
+    if (this.#tempId !== "ytThumb" && this.#tempId !== "ytFrame") return;
+
+    this.#vidTitre = this.#vidElement.querySelector(".vidTitre");
+
+    // Déterminer si c'est une vidéo ou un diaporama
+    this.#vidClass =
+      this.#vidItem.clas.slice(0, 4) === ".vid" ? "video" : "diapo";
+
+    // Formater le texte du titre
+    const typeCapitalized =
+      this.#vidClass.charAt(0).toUpperCase() + this.#vidClass.slice(1);
+    this.#vidTitre.textContent = `${typeCapitalized} ${this.#vidItem.text}`;
+    this.#vidTitre.classList.add(this.#vidClass);
+  }
+
+  /**
+   * Configure l'élément vidéo ou thumbnail
+   * @private
+   */
+  #configurerVideo() {
     this.#video = this.#vidElement.querySelector(".vidImg");
     this.#video.setAttribute("data-id", this.#vidItem.id);
+
     if (this.#tempId === "ytThumb") {
+      // Pour les miniatures, on utilise l'image de prévisualisation
       this.#video.setAttribute(
         "src",
         `https://img.youtube.com/vi/${this.#vidItem.id}/maxresdefault.jpg`
       );
     } else {
+      // Pour les iframes, on configure la source selon qu'il s'agisse d'une vidéo ou d'une playlist
       this.#video.setAttribute("title", this.#vidItem.text);
-      this.#video.setAttribute(
-        "src",
-        this.#vidItem.id.length !== 34
-          ? `https://www.youtube-nocookie.com/embed/${
-              this.#vidItem.id
-            }?rel=0&autoplay=0`
-          : `https://www.youtube-nocookie.com/embed/videoseries?list=${
-              this.#vidItem.id
-            }&rel=0&autoplay=0`
-      );
+
+      const isPlaylist = this.#vidItem.id.length === 34;
+      const baseUrl = "https://www.youtube-nocookie.com/embed/";
+      const videoUrl = `${baseUrl}${this.#vidItem.id}?rel=0&autoplay=0`;
+      const playlistUrl = `${baseUrl}videoseries?list=${
+        this.#vidItem.id
+      }&rel=0&autoplay=0`;
+
+      this.#video.setAttribute("src", isPlaylist ? playlistUrl : videoUrl);
     }
   }
+
+  /**
+   * Récupère l'élément vidéo créé
+   * @return {HTMLElement} Élément vidéo
+   */
   get retourItem() {
     return this.#vidElement;
   }
 }
+
+/**
+ * Crée un élément barre pour la navigation
+ * @class BarItem
+ */
 class BarItem {
+  /** @type {VideoItem} - Objet vidéo */
   #vidObj;
+  /** @type {HTMLElement} - Élément barre créé */
   #barElement;
-  constructor(vid) {
-    this.#vidObj = vid;
+
+  /**
+   * Crée un élément barre
+   * @param {VideoItem} video - Objet vidéo
+   */
+  constructor(video) {
+    this.#vidObj = video;
     this.#barElement = cloneTemplate("line").firstElementChild;
+
+    // Configurer l'élément
     this.#barElement.textContent = this.#vidObj.text;
     this.#barElement.classList.add("ytItem");
     this.#barElement.classList.add(this.#vidObj.clas.slice(1, 4));
   }
 
+  /**
+   * Récupère l'élément barre créé
+   * @return {HTMLElement} Élément barre
+   */
   get retourBarItem() {
     return this.#barElement;
   }
 }
+
+/**
+ * Crée un élément année pour la navigation
+ * @class AnnItem
+ */
 class AnnItem {
+  /** @type {string} - Année */
   #vidObj;
+  /** @type {HTMLElement} - Élément année créé */
   #annElement;
-  constructor(vid) {
-    this.#vidObj = vid;
+
+  /**
+   * Crée un élément année
+   * @param {string} annee - Année à afficher
+   */
+  constructor(annee) {
+    this.#vidObj = annee;
     this.#annElement = cloneTemplate("line").firstElementChild;
+
+    // Configurer l'élément
     this.#annElement.textContent = this.#vidObj;
     this.#annElement.dataset.year = this.#vidObj;
     this.#annElement.dataset.select = ".ann";
   }
 
+  /**
+   * Récupère l'élément année créé
+   * @return {HTMLElement} Élément année
+   */
   get retourAnnItem() {
     return this.#annElement;
   }
